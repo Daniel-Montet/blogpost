4466938b0571c196485d06c4cb9148ffb85bbfbe
Daniel Montet <dmontetproff@gmail.com>
2019-04-30 13:11:58 +0300

    Remove uploads module'

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,6 @@ login_manager.login_message_category='info'
 
 mail = Mail()
 
-photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
     app= Flask(__name__)
     app.config.from_object(config_options[config_name])
