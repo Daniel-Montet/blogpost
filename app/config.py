a93d1f94679869433a041508d4ef8115e057a61a
Daniel Montet <dmontetproff@gmail.com>
2019-04-30 14:22:37 +0300

    Rename db

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY='fe80d26ee9e9e49b181c65aee655938c'
-    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://daniel:dominuslew@localhost/pitchit'
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://daniel:dominuslew@localhost/blog'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
